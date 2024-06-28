7aef102e01da108d7c7200e10f9c9ba5b08687d6
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-06-28 12:31:07 +0200

    initialization

---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Skills = () =>{
+    return(
+        <div>
+            Skills
+        </div>
+    )
+}
+
+export default Skills;
---
+++ src/components/WorkExperience.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+
+const WorkExperience = () =>{
+    return(
+        <div>
+            WorkExperience
+        </div>
+    )
+}
+
+export default WorkExperience;
